4a21d813ac7f3f646a46c92c50764d776a31e109
wasif <wasif.aleem@braincrop.net>
2024-01-30 14:58:55 +0500

    update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let port = 8081
 console.log("result", 2+2)
 
 app.get('/', (req, res,next)=>{
-    res.json({msg: `hello world`, project: "express"})
+    res.json({msg: `hello `, project: "express"})
 })
 // app.get('/', (req, res,next)=>{
 //     res.sendFile(path.join(__dirname,"index.html"))
