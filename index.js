725039079805b14ad9ab9d8ccbab50b06e3814ef
wasif <wasif.aleem@braincrop.net>
2024-03-29 15:48:34 +0500

    chore: code formatting

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const path = require("path")
+const path = require("path");
 
-let port = 8081
-console.log("result", 2+2)
+let port = 8081;
+console.log("result", 2 + 2);
 
-app.get('/', (req, res,next)=>{
-    res.json({msg: `tuesday`, project: "express"})
-})
+app.get("/", (req, res, next) => {
+  res.json({ msg: `tuesday`, project: "express" });
+});
 // app.get('/', (req, res,next)=>{
 //     res.sendFile(path.join(__dirname,"index.html"))
 // })
 
-app.listen(port, ()=>{
-    console.log(`server is listneing on port ${port}`)     
-})
-    
+app.listen(port, () => {
+  console.log(`server is listneing on port ${port}`);
+});
